73810c650833946653b944134c277d59967e1c64
Florian Marin <flomarin88@users.noreply.github.com>
2015-11-27 10:21:45 +0100

    Aliases updated

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -14,7 +14,6 @@ apps=(
     spotify
     sublime-text3
     tunnelblick
-    transmit
     virtualbox
     vagrant
     vagrant-manager
@@ -25,3 +24,8 @@ brew cask install "${apps[@]}"
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package && qlmanage -r
+
+# rbenv install 2.2.3
+# rbenv global 2.2.3
+# gem install bundler
+# gem install dashing
