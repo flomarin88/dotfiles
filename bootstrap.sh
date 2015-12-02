68deff047443e6a088c27631d11e814cee6b6daa
Florian Marin <flomarin88@users.noreply.github.com>
2015-12-02 11:59:22 +0100

    Intellij + Tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 ln -s ~/dotfiles/exports .exports
 ln -s ~/dotfiles/aliases .aliases
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.tmuxinator ~/dotfiles/tmux
+ln -s ~/dotfiles/tmux ~/.tmuxinator 
